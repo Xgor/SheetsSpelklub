6bf5f6dd4a61272ee60afc53df22c4b04ab18fd7
Xgor <real.xgor@hotmail.com>
2024-08-21 23:39:31 +0200

    Added support for an extra vote system

--- main.py
+++ main.py
@@ -1,27 +1,41 @@
 import openpyxl
 import random
 from tkinter.filedialog import askopenfilename
-# Gets name for the selected row in 
-def get_game_name(worksheet,number):
-    cell_name = "{}{}".format("A", number)
-    if worksheet != None:
-        print(worksheet[cell_name].value)
 
 def main():
-    filename = askopenfilename()
+    # Fråga efter excel dokument
+    filename = askopenfilename( filetypes =[('Excel files', '*.xlsx')])
     wb = openpyxl.load_workbook(filename)
 
+    # Öppnar spellistan
     first_sheet = wb.sheetnames[0]
     worksheet = wb[first_sheet]
 
-    game_count = worksheet.max_row
+    # Skapar en array av alla spel
+    games = []
+    for i in range(2, worksheet.max_row):
+        g = worksheet["{}{}".format("A", i)].value
+        if g != None:
+            games.append(g)
+            # Lägger till extra kopior om det har lagts extra röster in i H kolumnen
+            copies = worksheet["{}{}".format("H", i)].value
+            if copies == None: copies = 0
+            for j in range(int(copies)):
+                games.append(g)
 
     print('Hur många spel vill du ha:')
-    x = input()
-    selected_games = random.sample(range(2, game_count), int(x))
+    game_count = input()
+    selected_games = []
+    # Lägg in antalet spel valda
+    for i in range(int(game_count)):
+        game = games[random.randrange(2, len(games))]
+        selected_games.append(game)
+        # Tar bort duplikationerna av valda spelet i listan
+        games = [j for j in games if j != game]
+        pass
 
     for game in selected_games:
-        get_game_name(worksheet,game)
+        print(game)
     x = input()
 
 if __name__ == "__main__":
